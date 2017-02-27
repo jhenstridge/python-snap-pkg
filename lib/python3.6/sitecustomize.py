3584fcce444a1fbbae68109a0900ace3e339c272
James Henstridge <james@jamesh.id.au>
2017-02-27 12:19:17 +0800

    Update sitecustomize to set site.USER_SITE to a directory under $SNAP_USER_COMMON, and add it to sys.path.

--- lib/python3.6/sitecustomize.py
+++ lib/python3.6/sitecustomize.py
@@ -1,9 +1,24 @@
 import os
-import sys
 import site
+import sys
 
 def main():
-    if 'SNAP' not in os.environ:
+    snap = os.environ.get('SNAP')
+    if snap is None:
         return
-    snap = os.environ['SNAP']
-    site.addsitepackages(None, [snap])
+
+    known_paths = None
+    # Add a user site directory if we are running in the context of
+    # the Python snap (i.e. not being used through the content interface).
+    # We detect this by checking to see if sys.prefix == $SNAP.
+    if snap == sys.prefix:
+        snap_user_common = os.environ.get('SNAP_USER_COMMON')
+        if snap_user_common is not None and site.ENABLE_USER_SITE:
+            site.USER_BASE = snap_user_common
+            site.USER_SITE = os.path.join(
+                site.USER_BASE, 'lib', 'python%d.%d' % sys.version_info[:2])
+            if os.path.isdir(site.USER_SITE):
+                known_paths = site.addsitedir(site.USER_SITE, known_paths)
+    site.addsitepackages(known_paths, [snap])
+
+main()
